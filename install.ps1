5fb6cb08cc0817f20685ab0a76c905dda712043c
Vee <elijah@wesnet.link>
2025-01-29 20:36:24 +1100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
  
 #Install Packages with Chocolatey
-choco install cinebench 7zip vlc discord googlechrome firefox ubisoft-connect epicgameslauncher steam spotify wget -y
+choco install cinebench 7zip vlc discord googlechrome firefox ubisoft-connect epicgameslauncher steam spotify wget sdio -y
 
 <# Set Dns (Just testing)
 netsh interface ipv4 set dnsservers "Wi-Fi" static 1.1.1.1 primary
@@ -13,3 +13,6 @@ netsh interface ipv4 set dnsservers "Wi-Fi" static 3.3.3.3 index=2
 netsh interface ipv4 set dnsservers "Ethernet" static 1.1.1.1 primary
 netsh interface ipv4 set dnsservers "Ethernet" static 3.3.3.3 index=2
  #>
+
+
+ Write-Output done
