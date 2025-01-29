9035d34a81c8734954571d57bbae86340805bf7f
Vee <elijah@wesnet.link>
2025-01-29 19:28:29 +1100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 #Start of Install Script
 
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
  
 #Install Packages with Chocolatey
 choco install cinebench 7zip vlc discord googlechrome ubisoft-connect epicgameslauncher steam spotify -y
