e5defe8dfea6e5e5ef1cb7b7d78676c2a051f7b5
Vee <elijah@wesnet.link>
2025-01-30 08:21:26 +1100

    prototyping menu

--- install.ps1
+++ install.ps1
@@ -1,5 +1,45 @@
 #Start of Install Script
 
+$MainMenu = {
+    Write-Host " ***************************"
+    Write-Host " *           Menu          *"
+    Write-Host " ***************************"
+    Write-Host
+    Write-Host " 1.) Install Install Chocolatey"
+    Write-Host " 2.) Install Applications"
+    Write-Host " 3.) Set DNS"
+    Write-Host " 4.) Everything"
+    Write-Host " 5.) Quit"
+    Write-Host
+    Write-Host " Select an option and press Enter: "  -nonewline
+    }
+    cls
+
+
+
+    Do {
+        cls
+        Invoke-Command $MainMenu
+        $Select = Read-Host
+        Switch ($Select)
+            {
+            1 {
+               echo Commands to be run if user selects menu option 1
+               }
+            2 {
+                Commands to be run if user selects menu option 2
+               }
+            3 {
+                Commands to be run if user selects menu option 3
+               }
+            4 {
+                Commands to be run if user selects menu option 4
+               }
+            }
+        }
+        While ($Select -ne 5)
+        
+        
 #Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
  
